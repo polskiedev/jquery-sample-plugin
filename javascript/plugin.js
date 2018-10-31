d34d4b97c2646f083977c37f62c9d948b9a1885a
polskiedev <polskie.dev@gmail.com>
2018-11-01 02:53:02 +0800

    ReCoding

--- javascript/plugin.js
+++ javascript/plugin.js
@@ -1,49 +1,91 @@
-window.APPLICATION_LAYER_IDS = [];
 
- function ApplicationLayer(layer_id, config) {
-    var isLayerExists = false;
-    var verbose = true;
-    var $this = this;
+function LayerBuilder(layer_id, config){
+    this.plugin = {
+        name: 'LayerBuilder',
+        code: 'LayerBuilder',
+        version: '1.0.0'
+    };
 
-    this.pluginName = 'ApplicationLayer';
-    this.pluginVersion = '1.0.0';
+    this._id = layer_id;
+    this.layer_id = this._id;
 
-    this.debug = function (title, message){
-        if(verbose) {
-            console.log(this.pluginName+'@'+title+': '+message);
+    this._bVerbose = true;
+    this._bError = false;
+    this._defaultConfig = {};
+    this._userConfig = config;
+
+    this._log = function(message, noPrefix) {
+        if(this._bVerbose) {
+            var printMsg = message;
+            if(!noPrefix) {
+                printMsg = this.plugin.name + '> '+printMsg;
+            }
+            console.log(printMsg);
         }
     };
 
-    //Validate Layer
-    if(window.APPLICATION_LAYER_IDS.indexOf(layer_id) == -1) {
-        window.APPLICATION_LAYER_IDS.push(layer_id);
-        isLayerExists = false;
-        this.debug('Create Layer', layer_id);
-    } else {
-        isLayerExists = true;
-        this.debug('Existing Layer', layer_id);
-    }
+    this._plugin_init = function() {
+        if(!window.PLUGIN) {
+            window.PLUGIN = [];
+        }
 
-    //Set configuration
-    var defaultConfig = {
-        'title':'Title',
-        'content': 'Hello World!'
+        if(!window.PLUGIN[this.plugin.code]) {
+            window.PLUGIN[this.plugin.code] = [];
+        }
+
+        if(!window.PLUGIN[this.plugin.code][this._id]) {
+           window.PLUGIN[this.plugin.code][this._id] = {id: this._id, obj: this};
+        } else {
+            this._bError = true;
+            this._log('Layer ID `' + this._id + '` already exists. Please check your configuration.');
+        }
     };
-    
-    this.config = $.extend( {}, defaultConfig, config );
-    
-    if(isLayerExists) {
-        //selectors
-    } else {
-        //create doms
-    }
 
-    return $this;
+    this._init = function() {
+        this._plugin_init();
+        this.config = $.extend( {}, this._defaultConfig, this._userConfig );
+    };
 
-};
+    this.getInstanceIds = function() {
+        return Object.keys(window.PLUGIN[this.plugin.code]);
+    };
 
-$.extend(ApplicationLayer.prototype, {
-    'getInstanceIds': function(){
-        console.log(window.APPLICATION_LAYER_IDS);
-    }
+    this.getAllInstance = function() {
+        return window.PLUGIN[this.plugin.code];
+    };
+
+    this._init();
+   
+    return this;
+}
+
+$.extend(LayerBuilder.prototype, {
+    user_interface: function() {
+        var $this = this;
+
+        $.extend($this, {
+            '$ele': {},
+            'template': function() {
+                var parentWrapper = $('<div/>').attr({'id': 'plugin_'+$this.layer_id, 'class':'dialog-wrapper'});
+                var headerWrapper = $('<div/>').attr({'class': 'dialog-header'});
+                var contentWrapper = $('<div/>').attr({'class': 'dialog-content'});
+
+                parentWrapper.empty().append(headerWrapper).append(contentWrapper);
+                this.$ele.wrapper = parentWrapper;
+                return parentWrapper;
+            },
+            'render': function(){
+                this.template().appendTo('body');
+            }
+        });
+
+        return $this;
+    }  
 });
+// ,
+//     'behavior': {
+
+//     },
+//     'render': function(){
+//         this.user_interface.render();
+//     }
